414f2f665c6889057ffee7559080f65c5a8a3e31
Juma Antony <jumaantony840@gmail.com>
2023-02-23 15:17:40 +0300

    tweaked main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,20 +5,6 @@
 
 void positive_or_negative(int i);
 
-int main(void)
-{
-        int a, b, c;
-        int largest;
-
-        a = 972;
-        b = -98;
-        c = 0;
-
-        largest = largest_number(a, b, c);
-
-        printf("%d is the largest number\n", largest);
-
-        return (0);
-}
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */

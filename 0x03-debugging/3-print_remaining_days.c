1144d939416d2a93a3f8c2fd3fabf3751d887b5b
Juma Antony <jumaantony840@gmail.com>
2023-02-23 17:52:24 +0300

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,26 +12,59 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0)
-	{
-	if (month > 2 && day >= 60)
-	{
-	day++;
-	}
+    int days = 0;
 
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 366 - day);
-	}
-	else
-	{
-	if (month == 2 && day == 60)
-	{
-	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-	}
-	else
-	{
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
-	}
-	}
+    if (month < 1 || month > 12 || day < 1 || day > 31)
+    {
+        printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+        return;
+    }
+
+    switch (month)
+    {
+        case 12:
+            days += 30; /* Nov */
+        case 11:
+            days += 31; /* Oct */
+        case 10:
+            days += 30; /* Sep */
+        case 9:
+            days += 31; /* Aug */
+        case 8:
+            days += 31; /* Jul */
+        case 7:
+            days += 30; /* Jun */
+        case 6:
+            days += 31; /* May */
+        case 5:
+            days += 30; /* Apr */
+        case 4:
+            days += 31; /* Mar */
+        case 3:
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                days += 29;
+            else
+                days += 28; /* Feb */
+        case 2:
+            days += 31; /* Jan */
+            break;
+        case 1:
+            break;
+        default:
+            printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+            return;
+    }
+
+    if (day < 1 || day > days)
+    {
+        printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+        return;
+    }
+
+    days -= day;
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        printf("Day of the year: %d\nRemaining days: %d\n", 31 + 29 + days, days);
+    else
+        printf("Day of the year: %d\nRemaining days: %d\n", 31 + 28 + days, days);
 }

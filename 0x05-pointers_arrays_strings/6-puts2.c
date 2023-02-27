437eb96e727aa62bc102cc791ad9160cf5dd3432
Juma Antony <jumaantony840@gmail.com>
2023-02-27 11:45:15 +0300

    tweaked 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,25 +1,21 @@
-/**
- * puts2 - print character of a string
- *      in an array starting from the first
- *      character followed by the rest of the
- *      even index array
- *
- * @str: string input array pointer
- *
- * Return: nothing
-*/
+include 'main.h'
 
-void puts2(char *str)
-{
-	int i;
+  /** 
+   *puts2 - prints string, followed by a new line, 
+   * @str: pointer to the string to point 
+   * Return: void
+   */
+   int void_puts2(char *str) 
+  {
+      int i = 0;
 
-	for (i = 0; str[i] >= 0; i++)
-	{
-		if (str[i] == '\0')
-			break;
-
-		if (i % 2 == 0)
-			_putchar(str[i]);
-	}
-	_putchar('\n');
+     while (str[i] != '\0' ) 
+{   
+     if (i % 2 == 0) 
+   {
+        _putchar(str[i]);
+} 
+     i++;
+  } 
+     _putchar('\n');
 }

688e18daa4e3d0c9ca1b0e8f682f640207d75f14
Juma Antony <jumaantony840@gmail.com>
2023-04-14 12:59:33 +0300

    task one correction

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,7 +1,13 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
-
-
-#endif /* HOLBERTON_H */
+// void print_binary(unsigned long int n);
+// void _putchar(char c);
+// int get_bit(unsigned long int n, unsigned int index);
+// int set_bit(unsigned long int *n, unsigned int index);
+// int clear_bit(unsigned long int *n, unsigned int index);
+// unsigned int flip_bits(unsigned long int n, unsigned long int m);
+// int get_endianness(void);
+#endif /* _HOLBERTON_H_ */

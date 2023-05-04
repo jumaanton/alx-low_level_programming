5e4454ede13f3939d0bce3cc7a6386c8b60284e2
Juma Antony <jumaantony840@gmail.com>
2023-05-04 17:35:13 +0300

    fixing 100 and 101 error

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * get_endianness - checks the endianness
- * 
- * return 0 if big endian 1 if little endian
+ * get_endianness - Checks the endianness.
+ *
+ * Return: If big-endian - 0.
+ *         If little-endian - 1.
  */
-
 int get_endianness(void)
 {
-	int n;
+	int num = 1;
+	char *endian = (char *)&num;
 
-	n=1;
-	if (*(char *)&n == 1)
+	if (*endian == 1)
 		return (1);
-	else
-		return (0);
+
+	return (0);
 }
-
-
